bf983ede313475c78ee8fa6bd416de7ad8252364
Lily Hopkins <lily@hpkns.uk>
2022-06-14 14:34:53 +0100

    Added multiple sources

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+const quoteSources = {
+    'Kanye': {
+        'url': 'https://api.kanye.rest',
+        'process': data => data.quote,
+    },
+    'Famous Quotes (Quotable API)': {
+        'url': 'https://api.quotable.io/random',
+        'process': data => `${data.content} &mdash; ${data.author}`,
+    }
+};
+
 const getQuote = (cb) =>
-    fetch('https://api.kanye.rest')
+    fetch(quoteSources[localStorage.getItem('quoteSource')].url)
         .then(res => res.json())
         .then(res => incrementQuoteCount(res))
+        .then(res => quoteSources[localStorage.getItem('quoteSource')].process(res))
         .then(data => cb(data));
 
 const getCat = (cb) =>
@@ -31,7 +43,33 @@ const background = document.querySelector('.background');
 const nextCat = () => getCat(data => background.style.backgroundImage = `url(${data})`);
 
 const quote = document.querySelector('.quote');
-const nextQuote = () => getQuote(data => quote.innerHTML = data.quote);
+const nextQuote = () => getQuote(data => quote.innerHTML = data);
+
+const sources = document.getElementById('sources');
+
+// Populate sources
+for (const key in quoteSources) {
+    if (Object.hasOwnProperty.call(quoteSources, key)) {
+        const element = quoteSources[key];
+        const option = document.createElement('option');
+        option.innerText = key;
+        sources.appendChild(option);
+    }
+}
+
+// Set default source
+if (localStorage.getItem('quoteSource') === null) {
+    localStorage.setItem('quoteSource', 'Kanye');
+}
+
+// Preselect source
+sources.value = localStorage.getItem('quoteSource');
+
+// Monitor source change
+sources.onchange = () => {
+    localStorage.setItem('quoteSource', sources.value);
+    nextQuote();
+};
 
 document.getElementById('nextCat').onclick = nextCat;
 document.getElementById('nextQuote').onclick = nextQuote;
